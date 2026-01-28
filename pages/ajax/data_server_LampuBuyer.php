049d52669bb33c8a592056fe734a590587b73014
anthomars <joko.susanto@indotaichen.com>
2026-01-28 12:00:17 +0700

    lampu buyer

--- pages/ajax/data_server_LampuBuyer.php
+++ pages/ajax/data_server_LampuBuyer.php
@@ -11,24 +11,59 @@ $columns = array(
     5 => 'create_by'
 );
 // set_order_type("desc");
-$sql = "SELECT id, buyer, flag, lampu, created_at, create_by FROM vpot_lampbuy";
-$query = mysqli_query($con,$sql) or die("data_server.php: get dataku");
-$totalData = mysqli_num_rows($query);
-$totalFiltered = $totalData;
-if (!empty($requestData['search']['value'])) {
-    $sql .= " where buyer LIKE '%" . $requestData['search']['value'] . "%' ";
-    $sql .= " OR lampu LIKE '%" . $requestData['search']['value'] . "%' ";
+$search = isset($requestData['search']['value']) ? trim($requestData['search']['value']) : '';
+$whereSql = '';
+$params = [];
+
+if ($search !== '') {
+    $whereSql = " WHERE buyer LIKE ? OR lampu LIKE ? ";
+    $like = '%' . $search . '%';
+    $params[] = $like;
+    $params[] = $like;
 }
-//----------------------------------------------------------------------------------
-$query = mysqli_query($con,$sql) or die("data_server.php: get dataku1");
-$totalFiltered = mysqli_num_rows($query);
-$sql .= " ORDER BY " . $columns[$requestData['order'][0]['column']] . "  " . $requestData['order'][0]['dir'] . "  LIMIT "
-    . $requestData['start'] . " ," . $requestData['length'] . "   ";
-$query = mysqli_query($con,$sql) or die("data_server.php: get dataku2");
+
+$totalData = 0;
+$countAll = sqlsrv_query($con, "SELECT COUNT(*) AS cnt FROM db_laborat.vpot_lampbuy");
+if ($countAll && ($row = sqlsrv_fetch_array($countAll, SQLSRV_FETCH_ASSOC))) {
+    $totalData = (int) $row['cnt'];
+}
+if ($countAll) {
+    sqlsrv_free_stmt($countAll);
+}
+
+$totalFiltered = $totalData;
+if ($whereSql !== '') {
+    $countFiltered = sqlsrv_query($con, "SELECT COUNT(*) AS cnt FROM db_laborat.vpot_lampbuy $whereSql", $params);
+    if ($countFiltered && ($row = sqlsrv_fetch_array($countFiltered, SQLSRV_FETCH_ASSOC))) {
+        $totalFiltered = (int) $row['cnt'];
+    }
+    if ($countFiltered) {
+        sqlsrv_free_stmt($countFiltered);
+    }
+}
+
+$orderColIndex = isset($requestData['order'][0]['column']) ? (int) $requestData['order'][0]['column'] : 0;
+$orderCol = isset($columns[$orderColIndex]) ? $columns[$orderColIndex] : 'id';
+$orderDir = (isset($requestData['order'][0]['dir']) && strtolower($requestData['order'][0]['dir']) === 'desc') ? 'DESC' : 'ASC';
+$start = isset($requestData['start']) ? (int) $requestData['start'] : 0;
+$length = isset($requestData['length']) ? (int) $requestData['length'] : 10;
+if ($length < 0) {
+    $length = 10;
+}
+
+$sql = "SELECT id, buyer, flag, lampu, created_at, create_by
+        FROM db_laborat.vpot_lampbuy $whereSql
+        ORDER BY $orderCol $orderDir
+        OFFSET ? ROWS FETCH NEXT ? ROWS ONLY";
+$dataParams = $params;
+$dataParams[] = $start;
+$dataParams[] = $length;
+
+$query = sqlsrv_query($con, $sql, $dataParams);
 //----------------------------------------------------------------------------------
 $data = array();
 $no = 1;
-while ($row = mysqli_fetch_array($query)) {
+while ($query && ($row = sqlsrv_fetch_array($query, SQLSRV_FETCH_ASSOC))) {
     $nestedData = array();
     $nestedData[] = $no++;
     $nestedData[] = $row["buyer"] . ' <button type="button" class="btn btn-xs btn-warning edit_lampu" attr-data="' . $row["buyer"] . '"><i class="fa fa-edit"></i></button>';
@@ -38,6 +73,9 @@ while ($row = mysqli_fetch_array($query)) {
     $nestedData[] = $row["create_by"];
 
     $data[] = $nestedData;
+}
+if ($query) {
+    sqlsrv_free_stmt($query);
 }
 //----------------------------------------------------------------------------------
 $json_data = array(
