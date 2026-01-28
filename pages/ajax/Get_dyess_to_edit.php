95a034550084a2ac4e26d19c5fff6e35fbf78915
anthomars <joko.susanto@indotaichen.com>
2026-01-28 11:09:44 +0700

    Migrate : Manage-Dyestuff

--- pages/ajax/Get_dyess_to_edit.php
+++ pages/ajax/Get_dyess_to_edit.php
@@ -3,19 +3,28 @@ ini_set("error_reporting", 1);
 session_start();
 include '../../koneksi.php';
 
-$sql_dyess = mysqli_query($con,"SELECT id, ket, code, code_new, `Product_Name`, Product_Unit, is_active from tbl_dyestuff where id = '$_POST[id]' LIMIT 1");
-$data = mysqli_fetch_array($sql_dyess);
+$id = isset($_POST['id']) ? (int) $_POST['id'] : 0;
+$data = null;
+
+$sql = "SELECT id, ket, code, code_new, Product_Name, Product_Unit, is_active
+        FROM db_laborat.tbl_dyestuff
+        WHERE id = ?";
+$stmt = sqlsrv_query($con, $sql, [$id]);
+if ($stmt) {
+    $data = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
+    sqlsrv_free_stmt($stmt);
+}
 
 //----------------------------------------------------------------------------------
 //----------------------------------------------------------------------------------
 $json_data = array(
-    "id" => $data['id'],
-    "ket" => $data['ket'],
-    "code" => $data['code'],
-	"code_new" => $data['code_new'],
-    "Product_Name" => $data['Product_Name'],
-    "Product_Unit" => $data['Product_Unit'],
-    "is_active" => $data['is_active']
+    "id" => $data ? $data['id'] : null,
+    "ket" => $data ? $data['ket'] : null,
+    "code" => $data ? $data['code'] : null,
+	"code_new" => $data ? $data['code_new'] : null,
+    "Product_Name" => $data ? $data['Product_Name'] : null,
+    "Product_Unit" => $data ? $data['Product_Unit'] : null,
+    "is_active" => $data ? $data['is_active'] : null
 );
 //----------------------------------------------------------------------------------
 echo json_encode($json_data);
--- pages/ajax/data_server_dyestuff.php
+++ pages/ajax/data_server_dyestuff.php
@@ -12,24 +12,59 @@ $columns = array(
     6 => 'is_active'
 );
 // set_order_type("desc");
-$sql = "SELECT id, ket, code, code_new, Product_Name, liquid_powder, Product_Unit, is_active FROM tbl_dyestuff";
-$query = mysqli_query($con,$sql) or die("data_server.php: get dataku");
-$totalData = mysqli_num_rows($query);
-$totalFiltered = $totalData;
-if (!empty($requestData['search']['value'])) {
-    $sql .= " where code LIKE '%" . $requestData['search']['value'] . "%' ";
-    $sql .= " OR Product_Name LIKE '%" . $requestData['search']['value'] . "%' ";
+$search = isset($requestData['search']['value']) ? trim($requestData['search']['value']) : '';
+$whereSql = '';
+$params = [];
+
+if ($search !== '') {
+    $whereSql = " WHERE code LIKE ? OR Product_Name LIKE ? ";
+    $like = '%' . $search . '%';
+    $params[] = $like;
+    $params[] = $like;
 }
-//----------------------------------------------------------------------------------
-$query = mysqli_query($con,$sql) or die("data_server.php: get dataku1");
-$totalFiltered = mysqli_num_rows($query);
-$sql .= " ORDER BY " . $columns[$requestData['order'][0]['column']] . "  " . $requestData['order'][0]['dir'] . "  LIMIT "
-    . $requestData['start'] . " ," . $requestData['length'] . "   ";
-$query = mysqli_query($con,$sql) or die("data_server.php: get dataku2");
+
+$totalData = 0;
+$countAll = sqlsrv_query($con, "SELECT COUNT(*) AS cnt FROM db_laborat.tbl_dyestuff");
+if ($countAll && ($row = sqlsrv_fetch_array($countAll, SQLSRV_FETCH_ASSOC))) {
+    $totalData = (int) $row['cnt'];
+}
+if ($countAll) {
+    sqlsrv_free_stmt($countAll);
+}
+
+$totalFiltered = $totalData;
+if ($whereSql !== '') {
+    $countFiltered = sqlsrv_query($con, "SELECT COUNT(*) AS cnt FROM db_laborat.tbl_dyestuff $whereSql", $params);
+    if ($countFiltered && ($row = sqlsrv_fetch_array($countFiltered, SQLSRV_FETCH_ASSOC))) {
+        $totalFiltered = (int) $row['cnt'];
+    }
+    if ($countFiltered) {
+        sqlsrv_free_stmt($countFiltered);
+    }
+}
+
+$orderColIndex = isset($requestData['order'][0]['column']) ? (int) $requestData['order'][0]['column'] : 0;
+$orderCol = isset($columns[$orderColIndex]) ? $columns[$orderColIndex] : 'id';
+$orderDir = (isset($requestData['order'][0]['dir']) && strtolower($requestData['order'][0]['dir']) === 'desc') ? 'DESC' : 'ASC';
+$start = isset($requestData['start']) ? (int) $requestData['start'] : 0;
+$length = isset($requestData['length']) ? (int) $requestData['length'] : 10;
+if ($length < 0) {
+    $length = 10;
+}
+
+$sql = "SELECT id, ket, code, code_new, Product_Name, liquid_powder, Product_Unit, is_active
+        FROM db_laborat.tbl_dyestuff $whereSql
+        ORDER BY $orderCol $orderDir
+        OFFSET ? ROWS FETCH NEXT ? ROWS ONLY";
+$dataParams = $params;
+$dataParams[] = $start;
+$dataParams[] = $length;
+
+$query = sqlsrv_query($con, $sql, $dataParams);
 //----------------------------------------------------------------------------------
 $data = array();
 $no = 1;
-while ($row = mysqli_fetch_array($query)) {
+while ($query && ($row = sqlsrv_fetch_array($query, SQLSRV_FETCH_ASSOC))) {
     if ($row["Product_Unit"] == 1) {
         $uom = "%";
     } elseif ($row["Product_Unit"] == 0) {
@@ -54,6 +89,9 @@ while ($row = mysqli_fetch_array($query)) {
     $nestedData[] = '<button class="btn btn-sm btn-warning dyess_edit" id="' . $row["id"] . '"><i class="fa fa-edit"></i></button>';
 
     $data[] = $nestedData;
+}
+if ($query) {
+    sqlsrv_free_stmt($query);
 }
 //----------------------------------------------------------------------------------
 $json_data = array(
