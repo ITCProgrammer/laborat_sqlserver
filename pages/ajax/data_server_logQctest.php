0e8ca4f48de1aefbffe56f808216aa55ec61331d
anthomars <joko.susanto@indotaichen.com>
2026-01-29 11:09:04 +0700

    migration: Log QC Test

--- pages/ajax/data_server_logQctest.php
+++ pages/ajax/data_server_logQctest.php
@@ -9,32 +9,68 @@ $columns = array(
     1 => 'no_counter'
 );
 
-$sql = "SELECT MAX(id) as maxid, no_counter FROM log_qc_test";
+$search = isset($requestData['search']['value']) ? trim($requestData['search']['value']) : '';
+$whereSql = '';
+$params = [];
 
-$query = mysqli_query($con, $sql) or die("data_server.php: failed to fetch data");
-$totalData = mysqli_num_rows($query);
-
-$totalFiltered = $totalData;
-
-if (!empty($requestData['search']['value'])) {
-    $sql .= " WHERE no_counter LIKE '%" . $requestData['search']['value'] . "%' ";
+if ($search !== '') {
+    $whereSql = " WHERE no_counter LIKE ? ";
+    $params[] = '%' . $search . '%';
 }
 
-$sql .= " GROUP BY no_counter ORDER BY " . $columns[$requestData['order'][0]['column']] . " " . $requestData['order'][0]['dir'] . " LIMIT "
-    . $requestData['start'] . "," . $requestData['length'];
+$totalData = 0;
+$countAll = sqlsrv_query($con, "SELECT COUNT(DISTINCT no_counter) AS cnt FROM db_laborat.log_qc_test");
+if ($countAll && ($row = sqlsrv_fetch_array($countAll, SQLSRV_FETCH_ASSOC))) {
+    $totalData = (int) $row['cnt'];
+}
+if ($countAll) {
+    sqlsrv_free_stmt($countAll);
+}
 
-$query = mysqli_query($con, $sql) or die("data_server.php: failed to fetch filtered data");
-$totalFiltered = mysqli_num_rows($query);
+$totalFiltered = $totalData;
+if ($whereSql !== '') {
+    $countFiltered = sqlsrv_query($con, "SELECT COUNT(DISTINCT no_counter) AS cnt FROM db_laborat.log_qc_test $whereSql", $params);
+    if ($countFiltered && ($row = sqlsrv_fetch_array($countFiltered, SQLSRV_FETCH_ASSOC))) {
+        $totalFiltered = (int) $row['cnt'];
+    }
+    if ($countFiltered) {
+        sqlsrv_free_stmt($countFiltered);
+    }
+}
+
+$orderColIndex = isset($requestData['order'][0]['column']) ? (int) $requestData['order'][0]['column'] : 0;
+$orderCol = isset($columns[$orderColIndex]) ? $columns[$orderColIndex] : 'maxid';
+$orderDir = (isset($requestData['order'][0]['dir']) && strtolower($requestData['order'][0]['dir']) === 'desc') ? 'DESC' : 'ASC';
+$start = isset($requestData['start']) ? (int) $requestData['start'] : 0;
+$length = isset($requestData['length']) ? (int) $requestData['length'] : 10;
+if ($length < 0) {
+    $length = 10;
+}
+
+$sql = "SELECT MAX(id) as maxid, no_counter
+        FROM db_laborat.log_qc_test
+        $whereSql
+        GROUP BY no_counter
+        ORDER BY $orderCol $orderDir
+        OFFSET ? ROWS FETCH NEXT ? ROWS ONLY";
+$dataParams = $params;
+$dataParams[] = $start;
+$dataParams[] = $length;
+
+$query = sqlsrv_query($con, $sql, $dataParams);
 
 $data = array();
 $no = 1;
 
-while ($row = mysqli_fetch_array($query)) {
+while ($query && ($row = sqlsrv_fetch_array($query, SQLSRV_FETCH_ASSOC))) {
     $nestedData = array();
     $nestedData[] = $no++;
     $nestedData[] = $row["no_counter"];
 
     $data[] = $nestedData;
+}
+if ($query) {
+    sqlsrv_free_stmt($query);
 }
 
 $json_data = array(
--- pages/ajax/data_server_qctest_detail.php
+++ pages/ajax/data_server_qctest_detail.php
@@ -4,21 +4,33 @@ include '../../koneksi.php';
 if (isset($_POST['no_counter'])) {
     $no_counter = $_POST['no_counter'];
 
-    $sql = "SELECT * FROM log_qc_test WHERE no_counter = '$no_counter'";
-    $query = mysqli_query($con, $sql) or die(mysqli_error($con));
+    $sql = "SELECT * FROM db_laborat.log_qc_test WHERE no_counter = ?";
+    $query = sqlsrv_query($con, $sql, [$no_counter]);
+    if (!$query) {
+        $errors = sqlsrv_errors();
+        echo $errors ? $errors[0]['message'] : 'Query gagal';
+        exit;
+    }
 
     $html = '';
     $no = 1;
-    while ($row = mysqli_fetch_array($query)) {
+    while ($row = sqlsrv_fetch_array($query, SQLSRV_FETCH_ASSOC)) {
+        $doAt = $row['do_at'];
+        if ($doAt instanceof DateTimeInterface) {
+            $doAt = $doAt->format('Y-m-d H:i:s');
+        } elseif ($doAt === null) {
+            $doAt = '';
+        }
         $html .= '<tr>';
         $html .= '<td>' . $no++ . '</td>';
         $html .= '<td>' . $row['status'] . '</td>';
         $html .= '<td>' . $row['info'] . '</td>';
         $html .= '<td>' . $row['do_by'] . '</td>';
-        $html .= '<td>' . $row['do_at'] . '</td>';
+        $html .= '<td>' . $doAt . '</td>';
         $html .= '<td>' . $row['ip_address'] . '</td>';
         $html .= '</tr>';
     }
 
+    sqlsrv_free_stmt($query);
     echo $html;
 }
