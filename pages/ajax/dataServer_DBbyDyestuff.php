9a1d6b91894e30766c687540ac5e1aa629cf67bd
anthomars <joko.susanto@indotaichen.com>
2026-02-02 11:57:52 +0700

    migration: Dyestuff_Utilization

--- pages/ajax/dataServer_DBbyDyestuff.php
+++ pages/ajax/dataServer_DBbyDyestuff.php
@@ -9,50 +9,63 @@ $columns = array(
     3 => 'nama',
     4 => 'no_resep'
 );
-$sql = "select a.id, a.id_status, a.kode, a.nama, b.no_resep
-from tbl_matching_detail a
-join tbl_matching b on a.id_matching = b.id
-join tbl_status_matching c on a.id_status = c.id
-where c.status = 'selesai' and c.approve='TRUE'
-group by a.id, b.no_resep
-order by a.kode, a.id desc";
 
-$query = mysqli_query($con,$sql) or die("data_server.php: get dataku 0");
-$totalData = mysqli_num_rows($query);
+$baseSql = "FROM db_laborat.tbl_matching_detail a
+            JOIN db_laborat.tbl_matching b on a.id_matching = b.id
+            JOIN db_laborat.tbl_status_matching c on a.id_status = c.id
+            WHERE c.status = 'selesai' and c.approve = 'TRUE' and a.kode <> '---'";
+
+$countSql = "SELECT COUNT(*) AS total FROM (
+                SELECT DISTINCT a.id, a.id_status, a.kode, a.nama, b.no_resep
+                $baseSql
+            ) x";
+$countStmt = sqlsrv_query($con, $countSql);
+$countRow = sqlsrv_fetch_array($countStmt, SQLSRV_FETCH_ASSOC);
+$totalData = (int)($countRow['total'] ?? 0);
 $totalFiltered = $totalData;
 
 //----------------------------------------------------------------------------------
-$sql = "select a.id, a.id_status, a.kode, a.nama, b.no_resep
-from tbl_matching_detail a
-join tbl_matching b on a.id_matching = b.id
-join tbl_status_matching c on a.id_status = c.id
-where c.status = 'selesai' and c.approve='TRUE' and NOT a.kode = '---' ";
+$params = [];
+$whereSearch = '';
 
 if (!empty($requestData['search']['value'])) {
     //----------------------------------------------------------------------------------
-    $sql .= " AND (a.kode LIKE '" . $requestData['search']['value'] . "%' ";
-    $sql .= " OR a.nama LIKE '" . $requestData['search']['value'] . "%' ";
-    $sql .= " OR b.no_resep LIKE '" . $requestData['search']['value'] . "%') ";
+    $term = $requestData['search']['value'] . '%';
+    $whereSearch = " AND (a.kode LIKE ? OR a.nama LIKE ? OR b.no_resep LIKE ?)";
+    $params = [$term, $term, $term];
 }
 //----------------------------------------------------------------------------------
-$query = mysqli_query($con,$sql) or die("data_server.php: get dataku 1");
+$countFilteredSql = "SELECT COUNT(*) AS total FROM (
+                        SELECT DISTINCT a.id, a.id_status, a.kode, a.nama, b.no_resep
+                        $baseSql $whereSearch
+                    ) x";
+$countFilteredStmt = sqlsrv_query($con, $countFilteredSql, $params);
+$countFilteredRow = sqlsrv_fetch_array($countFilteredStmt, SQLSRV_FETCH_ASSOC);
+$totalFiltered = (int)($countFilteredRow['total'] ?? 0);
 
-$totalFiltered = mysqli_num_rows($query);
-$sql .= " group by a.id, b.no_resep ORDER BY a." . $columns[$requestData['order'][0]['column']] . " ,a.id   " . $requestData['order'][0]['dir'] . " LIMIT " . $requestData['start'] . " , " . $requestData['length'] . "   ";
-// var_dump(print_r($sql));
-// die;
-$query = mysqli_query($con,$sql) or die("data_server.php: get dataku 2");
+$orderColIndex = (int)($requestData['order'][0]['column'] ?? 0);
+$orderCol = $columns[$orderColIndex] ?? 'id';
+$orderDir = strtoupper($requestData['order'][0]['dir'] ?? 'DESC') === 'ASC' ? 'ASC' : 'DESC';
+$start = (int)($requestData['start'] ?? 0);
+$length = (int)($requestData['length'] ?? 10);
+
+$sql = "SELECT DISTINCT a.id, a.id_status, a.kode, a.nama, b.no_resep
+        $baseSql $whereSearch
+        ORDER BY a.$orderCol $orderDir, a.id $orderDir
+        OFFSET ? ROWS FETCH NEXT ? ROWS ONLY";
+$paramsPaged = array_merge($params, [$start, $length]);
+$query = sqlsrv_query($con, $sql, $paramsPaged);
 
 $data = array();
 $no = 1;
-while ($row = mysqli_fetch_array($query)) {
+while ($row = sqlsrv_fetch_array($query, SQLSRV_FETCH_ASSOC)) {
     $nestedData = array();
     $nestedData[] = $no++;
-    $nestedData[] = $row["id"];
-    $nestedData[] = $row["id_status"];
-    $nestedData[] = $row["kode"];
-    $nestedData[] = $row["nama"];
-    $nestedData[] = $row["no_resep"];
+    $nestedData[] = $row["id"] ?? '';
+    $nestedData[] = $row["id_status"] ?? '';
+    $nestedData[] = $row["kode"] ?? '';
+    $nestedData[] = $row["nama"] ?? '';
+    $nestedData[] = $row["no_resep"] ?? '';
     $nestedData[] = '<a class="btn btn-xs btn-info" href="?p=Detail-status-approved&idm=' . $row['id_status'] . '"><i class="fa fa-link"></i></a>';
     $data[] = $nestedData;
 }
