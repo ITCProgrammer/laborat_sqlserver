d6313f5ad16efb85070fa4ada868478bd9b5c0c0
anthomars <joko.susanto@indotaichen.com>
2026-02-02 13:32:15 +0700

    migration: Arsip

--- pages/ajax/data_server_arsip.php
+++ pages/ajax/data_server_arsip.php
@@ -16,50 +16,71 @@ $columns = array(
     9 => 'no_po',
     10 => 'cocok_warna',
     11 => 'approve_at',
-	12 => 'tgl_arsip'
+    12 => 'tgl_arsip'
 );
-$sql = "SELECT a.id as id_status, a.created_at as tgl_buat_status, a.created_by as status_created_by, b.id as id_matching,
-        a.grp, a.matcher, a.idm, b.no_order, b.langganan, b.no_warna, b.warna, b.no_item, b.no_po, b.cocok_warna, a.approve_at, a.status, lsm.do_at as tgl_arsip
-        FROM tbl_status_matching a
-        JOIN tbl_matching b ON a.idm = b.no_resep
-        JOIN log_status_matching lsm ON a.idm = lsm.ids AND lsm.info ='Resep di arsipkan'
-        where a.status = 'arsip'";
-$query = mysqli_query($con,$sql) or die("data_server.php: get dataku");
-$totalData = mysqli_num_rows($query);
+
+$baseSql = "FROM db_laborat.tbl_status_matching a
+            JOIN db_laborat.tbl_matching b ON a.idm = b.no_resep
+            JOIN db_laborat.log_status_matching lsm ON a.idm = lsm.ids AND lsm.info = 'Resep di arsipkan'
+            WHERE a.status = 'arsip'";
+
+$countSql = "SELECT COUNT(*) AS total FROM (
+                SELECT a.id
+                $baseSql
+            ) x";
+$countStmt = sqlsrv_query($con, $countSql);
+$countRow = sqlsrv_fetch_array($countStmt, SQLSRV_FETCH_ASSOC);
+$totalData = (int)($countRow['total'] ?? 0);
 $totalFiltered = $totalData;
+
+$params = [];
+$whereSearch = '';
 if (!empty($requestData['search']['value'])) {
-    $sql .= " AND (a.idm LIKE '%" . $requestData['search']['value'] . "%' ";
-    $sql .= " OR b.warna LIKE '%" . $requestData['search']['value'] . "%' ";
-    $sql .= " OR b.no_warna LIKE '%" . $requestData['search']['value'] . "%' ";
-    $sql .= " OR b.no_order LIKE '%" . $requestData['search']['value'] . "%' ";
-	$sql .= " OR lsm.do_at LIKE '%" . $requestData['search']['value'] . "%' ";
-    $sql .= " OR b.no_item LIKE '%" . $requestData['search']['value'] . "%')";
+    $term = '%' . $requestData['search']['value'] . '%';
+    $whereSearch = " AND (a.idm LIKE ? OR b.warna LIKE ? OR b.no_warna LIKE ? OR b.no_order LIKE ? OR CONVERT(varchar(19), lsm.do_at, 120) LIKE ? OR b.no_item LIKE ?)";
+    $params = [$term, $term, $term, $term, $term, $term];
 }
-//----------------------------------------------------------------------------------
-$query = mysqli_query($con,$sql) or die("data_server.php: get dataku1");
-$totalFiltered = mysqli_num_rows($query);
-$sql .= " ORDER BY " . $columns[$requestData['order'][0]['column']] . "  " . $requestData['order'][0]['dir'] . "  LIMIT "
-    . $requestData['start'] . " ," . $requestData['length'] . "   ";
-$query = mysqli_query($con,$sql) or die("data_server.php: get dataku2");
-//----------------------------------------------------------------------------------
+
+$countFilteredSql = "SELECT COUNT(*) AS total FROM (
+                        SELECT a.id
+                        $baseSql $whereSearch
+                    ) x";
+$countFilteredStmt = sqlsrv_query($con, $countFilteredSql, $params);
+$countFilteredRow = sqlsrv_fetch_array($countFilteredStmt, SQLSRV_FETCH_ASSOC);
+$totalFiltered = (int)($countFilteredRow['total'] ?? 0);
+
+$orderColIndex = (int)($requestData['order'][0]['column'] ?? 0);
+$orderCol = $columns[$orderColIndex] ?? 'id_status';
+$orderDir = strtoupper($requestData['order'][0]['dir'] ?? 'DESC') === 'ASC' ? 'ASC' : 'DESC';
+$start = (int)($requestData['start'] ?? 0);
+$length = (int)($requestData['length'] ?? 10);
+
+$sql = "SELECT a.id as id_status, a.created_at as tgl_buat_status, a.created_by as status_created_by, b.id as id_matching,
+               a.grp, a.matcher, a.idm, b.no_order, b.langganan, b.no_warna, b.warna, b.no_item, b.no_po, b.cocok_warna, a.approve_at, a.status, lsm.do_at as tgl_arsip
+        $baseSql $whereSearch
+        ORDER BY $orderCol $orderDir
+        OFFSET ? ROWS FETCH NEXT ? ROWS ONLY";
+$paramsPaged = array_merge($params, [$start, $length]);
+$query = sqlsrv_query($con, $sql, $paramsPaged);
+
 $data = array();
 $no = 1;
-while ($row = mysqli_fetch_array($query)) {
+while ($row = sqlsrv_fetch_array($query, SQLSRV_FETCH_ASSOC)) {
     $nestedData = array();
-    $nestedData[] = $no . '. <input type="checkbox" id="' . $no . '" id_status="' . $row['id_status'] . '" id_matching="' . $row['id_matching'] . '" idm="' . $row['idm'] . '" no_order="' . $row['no_order'] . '">';
-    $nestedData[] = '<li class="btn-group" role="group"><a href="?p=Detail-status-wait-approve&idm=' . $row['id_status'] . '" class="btn btn-xs btn-info"><i class="fa fa-link"></i></a><button type="button" class="btn btn-xs btn-danger delete" id_status="' . $row['id_status'] . '" id_matching="' . $row['id_matching'] . '" idm="' . $row['idm'] . '" no_order="' . $row['no_order'] . '" why_batal="HAPUS_ARSIP"><i class="fa fa-trash"></i></button></li>';
-    $nestedData[] = $row["grp"];
-    $nestedData[] = $row["matcher"];
-    $nestedData[] = $row["idm"];
-    $nestedData[] = $row["no_order"];
-    $nestedData[] = $row["langganan"];
-    $nestedData[] = $row["warna"];
-    $nestedData[] = $row["no_warna"];
-    $nestedData[] = $row["no_item"];
-    $nestedData[] = $row["no_po"];
-    $nestedData[] = $row["cocok_warna"];
-    $nestedData[] = $row["approve_at"];
-	$nestedData[] = $row["tgl_arsip"];
+    $nestedData[] = $no . '. <input type="checkbox" id="' . $no . '" id_status="' . ($row['id_status'] ?? '') . '" id_matching="' . ($row['id_matching'] ?? '') . '" idm="' . ($row['idm'] ?? '') . '" no_order="' . ($row['no_order'] ?? '') . '">';
+    $nestedData[] = '<li class="btn-group" role="group"><a href="?p=Detail-status-wait-approve&idm=' . ($row['id_status'] ?? '') . '" class="btn btn-xs btn-info"><i class="fa fa-link"></i></a><button type="button" class="btn btn-xs btn-danger delete" id_status="' . ($row['id_status'] ?? '') . '" id_matching="' . ($row['id_matching'] ?? '') . '" idm="' . ($row['idm'] ?? '') . '" no_order="' . ($row['no_order'] ?? '') . '" why_batal="HAPUS_ARSIP"><i class="fa fa-trash"></i></button></li>';
+    $nestedData[] = $row["grp"] ?? '';
+    $nestedData[] = $row["matcher"] ?? '';
+    $nestedData[] = $row["idm"] ?? '';
+    $nestedData[] = $row["no_order"] ?? '';
+    $nestedData[] = $row["langganan"] ?? '';
+    $nestedData[] = $row["warna"] ?? '';
+    $nestedData[] = $row["no_warna"] ?? '';
+    $nestedData[] = $row["no_item"] ?? '';
+    $nestedData[] = $row["no_po"] ?? '';
+    $nestedData[] = $row["cocok_warna"] ?? '';
+    $nestedData[] = $row["approve_at"] ?? '';
+    $nestedData[] = $row["tgl_arsip"] ?? '';
 
     $data[] = $nestedData;
     $no++;
