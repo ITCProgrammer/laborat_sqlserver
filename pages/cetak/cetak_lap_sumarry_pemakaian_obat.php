d64e5b6830911ab324cb047776f250a896d68bd7
Asep Rifki <rifkipauji993@gmail.com>
2026-02-09 08:58:40 +0700

    fix ascending download excek penggunaan obat

--- pages/cetak/cetak_lap_sumarry_pemakaian_obat.php
+++ pages/cetak/cetak_lap_sumarry_pemakaian_obat.php
@@ -150,7 +150,7 @@ if (file_exists($logoPath)) {
     <?php
 
     $no = 1;
-    $sql = "SELECT * FROM db_laborat.tb_stock_gd_kimia WHERE ip_address = ?";
+    $sql = "SELECT * FROM db_laborat.tb_stock_gd_kimia WHERE ip_address = ? group by kode_obat ASC" ;
         $stmt = sqlsrv_query($con, $sql, [$ip_num]);
 
         if ($stmt === false) {
